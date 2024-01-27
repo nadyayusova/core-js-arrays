13b0503ce9aadcd0d083b044d32f746015357b47
nadyayusova <60035655+nadyayusova@users.noreply.github.com>
2024-01-27 22:51:02 +0300

    feat: do some tasks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -321,8 +321,8 @@ function flattenArray(nestedArray) {
  *   selectMany([[1, 2], [3, 4], [5, 6]], (x) => x) =>   [ 1, 2, 3, 4, 5, 6 ]
  *   selectMany(['one','two','three'], (x) => x.split('')) =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  return arr.flatMap(childrenSelector);
 }
 
 /**
@@ -338,8 +338,12 @@ function selectMany(/* arr, childrenSelector */) {
  *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
  *   calculateBalance([]) => 0
  */
-function calculateBalance(/* arr */) {
-  throw new Error('Not implemented');
+function calculateBalance(arr) {
+  if (arr.length === 0) {
+    return [];
+  }
+
+  return arr.reduce((acc, item) => acc + (item[0] - item[1]), 0);
 }
 
 /**
